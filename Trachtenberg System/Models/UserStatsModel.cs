fbd5988d9eb1d602b5f5217d3dba16b105dca48b
Ahm3d-KA <ahmed.kehinde2017@gmail.com>
2024-03-04 19:00:51 +0000

    start userstats model

---
+++ Trachtenberg System/Models/UserStatsModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Trachtenberg_System.Models;
+
+public struct Highscores
+{
+    public int MultiplicationEasyTest { get; set; }
+}
+// stores all the user stats and information for each user
+public class UserStatsModel
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    public string AccountName { get; set; }
+    [NotMapped]
+    public Highscores UserHighscores { get; set; }
+    
+}
